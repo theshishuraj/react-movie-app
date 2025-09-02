180ccff117792dd905d11b1bd7bf1666f79fb6c2
Shishu Raj <106923546+Craaaj@users.noreply.github.com>
2025-09-03 00:25:15 +0530

    First  Commit

---
+++ src/components/Search.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Search = ({searchTerm, setSearchTerm}) => {
+  return (
+    <div className='search'>
+      <div>
+        <img src="search.svg" alt="" />
+        <input 
+          type="text" 
+          placeholder='Search through thousands of movies'
+          value={searchTerm}
+          onChange={(ev)=> setSearchTerm(ev.target.value)}
+        />
+      </div>
+    </div>
+  )
+}
+
+export default Search
